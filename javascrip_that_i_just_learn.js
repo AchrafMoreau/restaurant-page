f306a3f66a24d7e09674432e820620fccd5841f6
unknown <elachraf6@gmail.com>
2023-01-07 18:29:11 +0100

    adding a trick whit javascript

---
+++ javascrip_that_i_just_learn.js
@@ -0,0 +1,22 @@
+
+
+let orderbtn = document.querySelectorAll('.botona');
+let modelcontent = document.getElementById("model-container");
+let closebtn = document.getElementById("close-btn");
+
+orderbtn.forEach(elm => {
+
+    elm.addEventListener('click', function(){
+        modelcontent.style.display = 'block';
+    });
+})
+
+window.addEventListener('click', function(e){
+    if (e.target === modelcontent){
+        modelcontent.style.display = 'none'
+    }
+})
+
+closebtn.addEventListener('click', function(){
+    modelcontent.style.display = 'none'
+})
